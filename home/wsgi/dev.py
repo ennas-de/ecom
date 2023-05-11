2609417fa81595f9e0c09f81fe2997662f76d62c
ennas-de <dev.abdulhakeem@gmail.com>
2023-05-11 15:55:00 +0100

    set dev env

--- home/wsgi/dev.py
+++ home/wsgi/dev.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# set dev environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.dev")
 
 application = get_wsgi_application()
